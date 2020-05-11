7ff1102ab8573527339b80727ee36920204cb2cd
dunja <dunjavulovic95@gmail.com>
2020-05-11 14:44:10 +0200

    react

---
+++ src/main/resources/static/precache-manifest.1867694221d7a372ec5421207270f841.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe2afd0affdb593c7596ae5ecc2aa98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97a5578d0e761f114c3b",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6632efa08e392d49d5eb",
+    "url": "/static/css/main.86ded62d.chunk.css"
+  },
+  {
+    "revision": "97a5578d0e761f114c3b",
+    "url": "/static/js/2.b73276f3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b73276f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6632efa08e392d49d5eb",
+    "url": "/static/js/main.79173113.chunk.js"
+  },
+  {
+    "revision": "2aaea2d878c7579c7253",
+    "url": "/static/js/runtime-main.907a5ee7.js"
+  }
+]);
